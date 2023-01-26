e9d2c2fb589eb15bbf495aecb81f5c9359e034b5
Luuccaass24 <al415409@uji.es>
2023-01-26 13:03:46 +0100

    sum-Version3

--- Sum/suma1.cc
+++ Sum/suma1.cc
@@ -1,12 +1,16 @@
 #include <iostream>
 
-int main(){
-    int num;
-    std::cout<<"Enter a number: ";
-    std::cin>>num;
+int Sum(int num){
     int sum=0;
     for (int i=1;i<=num;i++){
         sum+=i;
     }
-    std::cout<<"The sum from 1 to "<<num<<" is "<<sum<<std::endl;
+    return sum;
+}
+
+int main(){
+    int num;
+    std::cout<<"Enter a number: ";
+    std::cin>>num;
+    std::cout<<"The sum from 1 to "<<num<<" is "<<Sum(num)<<std::endl;
 }
