c912b0e7c53e08bf05096e7db843e5377a4dba5e
Luuccaass24 <al415409@uji.es>
2023-01-26 12:51:13 +0100

    sum-Version1

---
+++ Sum/suma1.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    int num;
+    std::cout<<"Enter a number: ";
+    std::cin>>num;
+    int sum=0;
+    std::cout<<"The sum from 1 to "<<num<<" is "<<sum<<std::endl;
+}
