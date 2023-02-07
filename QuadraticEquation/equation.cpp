17a9a463b778bc897561761f4a03cd2b4bc2f5a8
Lucas <luuccaass24@UBUNTU.20.myguest.virtualbox.org>
2023-02-07 12:51:33 +0100

    V1

---
+++ QuadraticEquation/equation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    double a, b, c, x1, x2;
+    std::cout<<"Enter a: ";
+    std::cin>>a;
+    std::cout<<"Enter b: "
+    std::cin>>b;
+    x1=0;
+    x2=0;
+    std::cout<<x1<<x2<<std::endl;
+}
+    
