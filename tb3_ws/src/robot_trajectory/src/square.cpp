ecf3142f9246724edee9dab2c07b3dad95e5668c
Luuccaass24 <al415409@uji.es>
2023-02-16 13:33:03 +0100

    pruebas

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -10,14 +10,29 @@ int main(int argc, char * argv[])
   auto node = rclcpp::Node::make_shared("square");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   geometry_msgs::msg::Twist message;
-  rclcpp::WallRate loop_rate(500ms);
+  rclcpp::WallRate loop_rate(10ms);
 
-  while (rclcpp::ok()) {
-    message.linear.x = 1.0; 
-    message.angular.z = 1.0;
+  for (int j=0;j<4;j++){
+    int i=0,n=100;
+    while (rclcpp::ok() && (i<n)){
+        i++;
+        message.linear.x = 0.1; 
+        message.angular.z = 0;
+        publisher->publish(message);
+        rclcpp::spin_some(node);
+        loop_rate.sleep();
+    message.linear.x = 0;
     publisher->publish(message);
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
+    i=0, n=1500;
+    }while (rclcpp::ok() && (i<n)){
+        message.linear.x = 0; 
+        message.angular.z = 0.1;
+        publisher->publish(message);
+        rclcpp::spin_some(node);
+        loop_rate.sleep();
+    }
+    message.angular.z = 0;
+    publisher->publish(message);
   }
   rclcpp::shutdown();
   return 0;
