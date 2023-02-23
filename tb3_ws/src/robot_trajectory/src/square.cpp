0f942257e48cfc579725aab1171e89c077e063c5
Luuccaass24 <al415409@uji.es>
2023-02-23 13:16:24 +0100

    square_length

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -11,6 +11,7 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("square");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  node->declare_parameter("square_length", 1.0);
   node->declare_parameter("linear_speed", 0.1);
   node->declare_parameter("angular_speed",M_PI/20);
   geometry_msgs::msg::Twist message;
@@ -18,9 +19,10 @@ int main(int argc, char * argv[])
 
   double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
   double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
+  double square_length = node->get_parameter("square_length").get_parameter_value().get<double>();
   for (int j=0;j<4;j++){
       
-    int i=0,n=1/(0.01 * linear_speed);
+    int i=0,n=square_length/(0.01 * linear_speed);
     while (rclcpp::ok() && (i<n)){
         message.linear.x = linear_speed;
         message.angular.z = 0.0;
