0f77915dec808249eee5bd3f6fb3e7a538e751b5
Lucas <luuccaass24@UBUNTU.20.myguest.virtualbox.org>
2023-02-27 13:13:49 +0100

    Eigen Version

--- ModeMedian/median.cpp
+++ ModeMedian/median.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <Eigen/Dense>
+
+using Eigen::VectorXd;
+
 
 int main(int argc, char** argv) {
+    VectorXd v;
     double m=0, s=0;
     int n=0,element;
-    std::vector<int> elements;
     std::cin >> element;
     while (not std::cin.eof()) {
-        elements.push_back(element);
+        v << element;
         n +=1;
         std::cin >> element;
     }
-    std::sort(elements);
-    if (elements.size()%2==0){
+    std::sort(v);
+    if (v.size()%2==0){
         std::cout<<"even"<<std::endl;
-        m = elements[elements.size()/2];
-        s = elements[elements.size()/2+1];
+        m = v[v.size()/2];
+        s = v[v.size()/2+1];
         std::cout << "Median: "<<m<<s<<std::endl;
     }else{
         std::cout<<"odd"<<std::endl;
-        m = elements[elements.size()/2];
+        m = v[v.size()/2];
         std::cout << "Median: "<<m<<std::endl;
     
     
