f3a78d48c5d3576a1007b81e27e36379bb91bedf
Lucas <luuccaass24@UBUNTU.20.myguest.virtualbox.org>
2023-02-28 12:17:02 +0100

    V5

--- Sum/MaxSumMatrix.cpp
+++ Sum/MaxSumMatrix.cpp
@@ -27,8 +27,17 @@ int main() {
         s(i) = m.row(i).sum();
         std::cout<<"The sum of row is: "<< s << std::endl;
     }
-    double MaxSumRow = s.maxCoeff();
-    std::cout<<"The max sum is: "<<std::endl;
+    double MaxSumRow= s.maxCoeff();
+    std::cout<<"The max sum of rows is: "<<std::endl;
+    for(int i=0;i<cols;i++){
+        s(i) = m.col(i).sum();
+        std::cout<<"The sum of rows is: "<< s << std::endl;
+    }
+    double MaxSumCol = s.maxCoeff();
+    std::cout<<"The max sum of columns is: "<<std::endl;
+    
+    double maxRowColSum = MaxSumRow > MaxSumCol ? MaxSumRow : MaxSumCol;
+    std::cout<<"The maximum sum of rows or columns is: "<<std::endl;
     return 0;
     
 }
