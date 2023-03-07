0138b60e87298bd85d7495bace332cd67af611dc
Lucas <luuccaass24@UBUNTU.20.myguest.virtualbox.org>
2023-03-07 19:32:36 +0100

    ajustando version

--- tb3_ws/src/robot_trajectory/src/square_odom.cpp
+++ tb3_ws/src/robot_trajectory/src/square_odom.cpp
@@ -26,6 +26,40 @@ int main(int argc, char * argv[]){
   auto subscription = node->create_subscription<nav_msgs::msg::Odometry>("odom", 10, topic_callback);
 
   rclcpp::spin(node);
+  
+  auto node1 = rclcpp::Node::make_shared("publisher");
+  auto publisher = node1->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  node1->declare_parameter("square_length", 1.0);
+  node1->declare_parameter("linear_speed", 0.1);
+  node1->declare_parameter("angular_speed",M_PI_2);
+  geometry_msgs::msg::Twist message;
+  rclcpp::WallRate loop_rate(10ms);
+
+  double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
+  double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
+  double square_length = node->get_parameter("square_length").get_parameter_value().get<double>();
+  for (int j=0;j<4;j++){
+      
+    int i=0,n=square_length/(0.01 * linear_speed);
+    while (rclcpp::ok() && (i<n)){
+        message.linear.x = linear_speed;
+        message.angular.z = 0.0;
+        publisher->publish(message);
+        rclcpp::spin_some(node1);
+        loop_rate.sleep();
+        i++;
+    }
+    i=0, n=M_PI_2/(0.01 * angular_speed);
+    while (rclcpp::ok() && (i<n)){
+        message.linear.x = 0.0; 
+        message.angular.z = 3.1416/20;
+        message.angular.z = angular_speed;
+        publisher->publish(message);
+        rclcpp::spin_some(node1);
+        loop_rate.sleep();
+        i++;
+    }
+  }
   rclcpp::shutdown();
   return 0;
 }
