6fb669a0757a1c1f2e848f90abd9aa330f5782f2
Luuccaass24 <al415409@uji.es>
2023-03-23 13:25:49 +0100

    wandering v4

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -6,20 +6,33 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <Eigen/Dense>
 
 using namespace std::chrono_literals;
+using  Eigen::VectorXd;
+VectorXd vect;
+
 std::vector<float> v;
+
 
 void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
     v = msg->ranges;
+    vect.conservativeResize(20);
+    int n=10;
     std::cout<<"----"<<std::endl;
     for(int i=0;i<10;i++){
         std::cout<<v[i]<<std::endl;
+        vect(i)=v[i];
     }
     std::cout<<"----"<<std::endl;
     for(int i=350;i<=359;i++){
         std::cout<<v[i]<<std::endl;
+        if (i<360){
+            vect(n)=v[i];
+            n++;
+        }
     }
+    std::cout<<"Min: "<<vect.minCoeff()<<std::endl;
 }
   
 int main(int argc, char * argv[]){
