13944e7990ea34dac18e22dd5d1f33d2cd37c37e
Luuccaass24 <al415409@uji.es>
2023-03-28 12:52:06 +0200

    diapo 29

---
+++ tb3_ws/install/obstacles/share/obstacles/launch/detector_test.launch.py
@@ -0,0 +1,17 @@
+form launch import LaunchDescription
+from launch_ros.actions import Node
+import launch.actions
+
+def generate_launch_description():
+    return LaunchDescription([
+        launch.actions.DeclareLaunchArguments('bagfile'),
+        launch.actions.ExecuteProcess(
+            cmd=['ros2', 'bag', 'play', launch.substitutions.LaunchConfiguration('bagfile')],
+            output='screen'
+        ),
+        Node(
+            package='obstacles',
+            executable='detector'
+            )
+    ])
+    
