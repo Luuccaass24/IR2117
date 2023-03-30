b7439763643e1ccf3544808bf10d82356854416c
Luuccaass24 <al415409@uji.es>
2023-03-30 13:26:49 +0200

    diapo 42

--- tb3_ws/src/obstacles/src/detector.cpp
+++ tb3_ws/src/obstacles/src/detector.cpp
@@ -28,7 +28,7 @@ void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
 int main(int argc, char * argv[]){
     rclcpp::init(argc,argv);
     auto node = rclcpp::Node::make_shared("detector");
-    auto subscription = node->create_subscription<sensor_msgs::msg::LaserScan>("scan", 10 ,topic_callback);
+    auto subscription = node->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10 ,topic_callback);
     publisher = node->create_publisher<example_interfaces::msg::Bool>("obstacle", 10);
     node->declare_parameter("obs_angle_min", -M_PI/8);
     node->declare_parameter("obs_angle_max", M_PI/8);
